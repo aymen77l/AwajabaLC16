990c934ee3442a1db9026c5b81e8dc8971d25b23
Lagha Aymen <aymen.lagha.sio@gmail.com>
2026-09-07 11:19:49 +0200

    implementation constructeur

--- repas.java
+++ repas.java
@@ -2,4 +2,10 @@ public class repas {
 	private int numero ;
 	private LocalDate date ;
 	
+	public repas(int numero, LocalDate date){
+		super();
+		this.numero = numero;
+		this.date=date;
+	}
 }
+ 
